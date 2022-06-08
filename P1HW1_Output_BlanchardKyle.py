09ebf619751414e8261bff5b1471891db2301536
Blanchak1448 <107142798+Blanchak1448@users.noreply.github.com>
2022-06-08 14:34:52 -0400

    Add files via upload
    P1HW1

---
+++ P1HW1_Output_BlanchardKyle.py
@@ -0,0 +1,8 @@
+user_num = int(input("Enter integer:\n"))
+print("You entered: %d"%user_num)
+print("%d squared is %d"%(user_num,user_num**2))
+print("And %d cubed is %d !!"%(user_num, user_num**3))
+user_num2 = int(input("Enter another integer:\n"))
+print("%d + %d is %d"%(user_num, user_num2,user_num+user_num2))
+print("%d * %d is %d"%(user_num, user_num2,user_num*user_num2))
+# Type your code here
